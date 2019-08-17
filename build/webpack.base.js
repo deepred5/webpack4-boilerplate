e4e456ea096284b6d8bc9a771330ee4ea7768ed9
deepred5 <948931744@qq.com>
2019-08-17 11:07:59 +0800

    update: 匹配jsx

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -6,7 +6,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader"
       },
