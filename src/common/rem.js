7e6edeeb770df2feca6ec27334a4b60b018b3717
tiancong <tiancong@hujiang.com>
2020-01-14 17:33:21 +0800

    update: eslint

--- src/common/rem.js
+++ src/common/rem.js
@@ -1,16 +1,16 @@
 // rem 调整
-(function () {
-  var timer = null;
+(function rem() {
+  let timer = null;
 
   // 根据屏幕大小及dpi调整缩放和大小
   function onResize() {
-    var width = Math.min(document.documentElement.clientWidth, 750);
-    var e = width / 7.5;
-    document.documentElement.style.fontSize = e + 'px';
-    var realitySize = parseFloat(window.getComputedStyle(document.documentElement).fontSize);
+    const width = Math.min(document.documentElement.clientWidth, 750);
+    let e = width / 7.5;
+    document.documentElement.style.fontSize = `${e}px`;
+    const realitySize = parseFloat(window.getComputedStyle(document.documentElement).fontSize);
     if (e !== realitySize) {
       e = e * e / realitySize;
-      document.documentElement.style.fontSize = e + 'px';
+      document.documentElement.style.fontSize = `${e}px`;
     }
   }
   window.addEventListener('resize', function () {
