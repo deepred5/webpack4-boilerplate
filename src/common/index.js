e3d3cf4daad56083ee14315073190e53ffeb3180
deepred5 <948931744@qq.com>
2020-09-21 15:03:24 +0800

    update: 动态加载vconsole

--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,33 @@
 /**
  * 多页面共用的全局方法
  * 1. 导入公共的index.scss
+ * 2. 更加url参数动态添加vconsole
  */
 
 import '@/styles/index.scss';
+
+if (/debug=1/i.test(window.location.search)) {
+  const loadJs = (src, callback) => {
+    const sc = document.createElement('script');
+    sc.type = 'text/javascript';
+    sc.src = src;
+    if (callback) {
+      if (document.addEventListener) {
+        sc.addEventListener("load", callback, false);
+      } else {
+        sc.onreadystatechange = () => {
+          if (/loaded|complete/.test(sc.readyState)) {
+            sc.onreadystatechange = null;
+            callback();
+          }
+        };
+      }
+    }
+    document.head.appendChild(sc);
+  }
+
+  loadJs('//cdn.bootcdn.net/ajax/libs/vConsole/3.3.4/vconsole.min.js', () => {
+    /* eslint-disable-next-line no-undef, no-new */
+    new VConsole();
+  });
+}
