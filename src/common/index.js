1823fdb9ade732ea85621402985c50484c02dc7f
deepred5 <948931744@qq.com>
2020-09-21 15:15:02 +0800

    update: 动态加载vconsole

--- src/common/index.js
+++ src/common/index.js
@@ -2,6 +2,7 @@
  * 多页面共用的全局方法
  * 1. 导入公共的index.scss
  * 2. 导入fastclick.js
+ * 3. 导入vconsole.js
  */
 
 import '@/styles/index.scss';
@@ -16,3 +17,28 @@ fastclick.prototype.focus = function (targetElement) {
 // eslint-disable-next-line compat/compat
 fastclick.attach(document.body);
 
+if (/debug=1/i.test(window.location.search)) {
+  const loadJs = (src, callback) => {
+    const sc = document.createElement('script');
+    sc.type = 'text/javascript';
+    sc.src = src;
+    if (callback) {
+      if (document.addEventListener) {
+        sc.addEventListener("load", callback, false);
+      } else {
+        sc.onreadystatechange = () => {
+          if (/loaded|complete/.test(sc.readyState)) {
+            sc.onreadystatechange = null;
+            callback();
+          }
+        };
+      }
+    }
+    document.head.appendChild(sc);
+  }
+
+  loadJs('//cdn.bootcdn.net/ajax/libs/vConsole/3.3.4/vconsole.min.js', () => {
+    /* eslint-disable-next-line no-undef, no-new */
+    new VConsole();
+  });
+}
